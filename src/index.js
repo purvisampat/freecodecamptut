317dafd1829707568e78a88e639cf0203dd43802
purvi.sampat19 <purvi.sampat19@gmail.com>
2020-10-28 13:15:51 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// const Greetings = () => {
+//   return React.createElement('h1', {}, 'hello world');
+// }
+const book1 = {
+  img: 'https://m.media-amazon.com/images/I/51Ap7cBapFL._AC_UY218_.jpg',
+  title: 'Gone With the Wind',
+  author: 'by Margaret Mitchell'
+}
+const book2 = {
+  img: 'https://images-eu.ssl-images-amazon.com/images/I/51UNiQJBLEL._AC_US218_FMwebp_QL70_.jpg',
+  title: 'Legend of Suheldev: The King Who Saved India',
+  author: 'by Amish'
+}
+function BookList() { //Component
+  return (
+    <section className='bookList'>
+      <Book img={book1.img} title={book1.title} author={book1.author}/>
+      <Book img={book2.img} title={book2.title} author={book2.author}/>
+    </section>
+  );
+}
+const Book = (props) => {
+  console.log(props);
+  return (
+    <article className='book'>
+      <img src={props.img} alt=' ' />
+      <h1>{props.title}</h1>
+      <h4>{props.author} </h4>
+    </article>
+  )
+}
+ReactDOM.render(<BookList/>, document.getElementById('root'))
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
